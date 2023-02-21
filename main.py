d767a90c0ea6f148c67200638d6f9574558b8a4e
polo <jpolo5678@gmail.com>
2023-02-21 17:25:24 -0400

    add title bar

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import flet 
 
 def main(page: flet.Page):
-  pass  #cuerpo del programa etc
 
-if __name__ = "__main__":
+  appbar = flet.AppBar(
+      title = flet.Text("Amando el Café", color="white", size=35),
+      bgcolor = "brown"
+      )
+
+  page.add(appbar)
+
+if __name__ == "__main__":
   flet.app(port=8226, target=main, view=flet.WEB_BROWSER)
